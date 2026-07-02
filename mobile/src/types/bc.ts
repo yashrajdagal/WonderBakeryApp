1797f2b442fb7398e4eb1d9ad0e3824835a55712
YashRajDagal <satoshikuro7@gmail.com>
2026-07-02 07:49:08 +0400

    BC sales order

--- mobile/src/types/bc.ts
+++ mobile/src/types/bc.ts
@@ -47,3 +47,34 @@ export interface BCItem {
 
 // When customer orders, we POST a corresponding sales order to BC. So the Bakery's operations team sees the app orders alongside the B2B orders in BC. Hence, the production can plan with this unified view accordingly.
 
+export interface BCSalesOrder {
+    // Auto-generated sales order no. in BC - "SO-2026-0207"
+    // This becomes Order.bcSalesOrderNo after order syncs.
+    No_: string;
+
+    // When a user registers, BC wil create a code number to record the order details of the user. which would help in trekking the user for the order delivery.
+    Sell_to_Customer_No_: string;
+    
+    // Customer Name - denormalised for quick display in BC without a join.
+    Sell_to_Customer_Name: string;
+
+    //When the order was placed. ISO format
+    Order_Date: string;
+
+    //When the customer wants it to be delivered. Maps to Order.deliverySlot
+    Request_Delivery_Date: string;
+
+    // Order Total value excluding VAT
+    Amount: number;
+
+    //Total Value including 5% VAT in UAE
+    Amount_Including_VAT: number;
+
+    //BCSalesOrder Status for Our OrderStatus:
+    // Open = Our "Placed" or "confirmed"
+    // Released = Our "Preparing" or "Ready"
+    // Pending = Special workflow for BC state.
+    // The adapter maps between these vocabs
+    Status: 'Open' | 'Released' | 'Pending'
+    
+}
