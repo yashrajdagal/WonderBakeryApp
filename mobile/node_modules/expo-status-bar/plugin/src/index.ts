9a2cbcbd1640ee9b9640ae18d5aea85300360b55
YashRajDagal <satoshikuro7@gmail.com>
2026-07-27 07:28:28 +0400

    chore: remove node_modules from tracking

--- mobile/node_modules/expo-status-bar/plugin/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { Props } from './withStatusBar';
-
-export default (props: Props = {}): [string, Props] => ['expo-status-bar', props];
--- mobile/node_modules/expo-status-bar/src/StatusBar.web.ts
+++
@@ -1,9 +0,0 @@
-export function StatusBar() {
-  return null;
-}
-
-StatusBar.setStyle = () => {};
-StatusBar.setHidden = () => {};
-
-export function setStatusBarStyle() {}
-export function setStatusBarHidden() {}
